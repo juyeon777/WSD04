6cc208ab6760d27cdc9068a51fb1bde10ae0a1be
juyeon <okk1829@naver.com>
2024-12-26 23:27:39 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/WSD04/' : '/', // 리포지토리 이름으로 설정
+});
